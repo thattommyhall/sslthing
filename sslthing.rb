5cfe94c2eca3f17b265749015ec8125aba168235
thattommyhall <thattommyhall@gmail.com>
2021-11-30 14:50:58 +0100

    fetch challenge from S3

--- sslthing.rb
+++ sslthing.rb
@@ -1,33 +1,74 @@
 # frozen_string_literal: true
 
+require 'logger'
 require 'powerdns_pipe'
+require 'uri'
+require 'net/http'
 
 pipe = PowerDNS::Pipe.new(banner: 'sslthing')
+DOMAINS = ['sslify.me', 'sslocal.io']
 
+NS_IP = '151.139.184.11'
 IP_REGEX = /^(\d{1,3})-(\d{1,3})-(\d{1,3})-(\d{1,3})\./
 
 def extract_ip(name)
   match = IP_REGEX.match(name)
-  if match
-    octets = match.captures
-    octets.each do |octet|
-      i = octet.to_i
-      if !(i < 255 and i >= 0)
-        return nil
-      end
-    end
-    octets.join('.')
+  return unless match
+
+  octets = match.captures[0..3]
+  octets.each do |octet|
+    i = octet.to_i
+    return nil unless i < 255 and i >= 0
   end
+  octets.join('.')
+end
+
+def soa(domain)
+  "ns.#{domain}. hostmaster.#{domain}. 1 600 300 1209600 60"
+end
+
+def response_for(username)
+  uri = URI("http://challenges.sslify.me.s3-website-eu-west-1.amazonaws.com/twitter/#{username}")
+  res = Net::HTTP.get_response(uri)
+  res.body if res.is_a?(Net::HTTPSuccess)
 end
 
 if __FILE__ == $PROGRAM_NAME
+  log = Logger.new('/var/log/powerdns-pipe.log', 'daily')
+  log.level = Logger::DEBUG
   pipe.run! do
-    name = question.name
-    if question.qtype == 'A' or question.qtype == 'ANY'
-      extracted = extract_ip(name)
-      if extracted
-        answer(name: name, content: extracted, type: 'A')
+    name = question.name.downcase
+    qtype = question.qtype
+    log.debug("#{qtype}: #{name}")
+
+    if name.start_with?('_acme-challenge') and %w[TXT ANY].include?(qtype)
+      match = /_acme-challenge.(?<username>[\w+]{1,15}).(?<domain>sslify.me|sslocal.io)/.match(name)
+      username = match[:username]
+      response = response_for(username)
+      answer(name: name, content: response, type: 'TXT', ttl: 120)
+    end
+
+    if DOMAINS.include?(name)
+      case qtype
+      when 'NS'
+        answer(name: name, content: "ns.#{name}", type: 'NS')
+      when 'SOA'
+        answer(name: name, content: soa(name), type: 'SOA')
+      when 'ANY'
+        answer(name: name, content: "ns.#{name}", type: 'NS')
+        answer(name: name, content: soa(name), type: 'SOA')
       end
+    end
+
+    name_servers = DOMAINS.map { |domain| "ns.#{domain}" }
+
+    if name_servers.include?(name) and %w[A ANY].include?(qtype)
+      answer(name: name, content: NS_IP, type: 'A')
+    end
+
+    extracted = extract_ip(name)
+    if extracted and %w[A ANY].include?(qtype)
+      answer(name: name, content: extracted, type: 'A')
     end
   end
 end
--- test_sslthing.rb
+++ test_sslthing.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'minitest/autorun'
+require 'resolv'
+
 require_relative 'sslthing'
 
 class SSLThingTest < MiniTest::Test
@@ -8,5 +10,50 @@ class SSLThingTest < MiniTest::Test
     assert_equal extract_ip('192-168-0-1.tth.things.something'), '192.168.0.1'
     assert_nil extract_ip('192-168-0-allaa.tth.things.something')
     assert_nil extract_ip('192-168-0-255.tth.things.something')
+  end
+
+  def test_fetch_challenge
+    assert_equal "SOME-STRING", response_for("testuser")
+  end
+end
+
+def get_A(name)
+  dns = Resolv::DNS.new(nameserver_port: [['127.0.0.1', 2000]])
+  result = dns.getresources(name, Resolv::DNS::Resource::IN::A)
+  if result.length > 0
+    result.first.address.to_s
+  end
+end
+
+def get_TXT(name)
+  dns = Resolv::DNS.new(nameserver_port: [['127.0.0.1', 2000]])
+  result = dns.getresources(name, Resolv::DNS::Resource::IN::TXT)
+  if result.length > 0
+    result.first.strings.first
+  end
+end
+
+def get_NS(name)
+  dns = Resolv::DNS.new(nameserver_port: [['127.0.0.1', 2000]])
+  result = dns.getresources(name, Resolv::DNS::Resource::IN::NS)
+  if result.length > 0
+    result.first.name.to_s
+  end
+end
+
+class SSLThingTest < MiniTest::Test
+  def test_ns_query
+    assert_equal 'ns.sslify.me', get_NS('sslify.me')
+    assert_equal 'ns.sslocal.io', get_NS('sslocal.io')
+  end
+
+  def test_a_lookups
+    assert_equal '1.2.3.4', get_A('1-2-3-4.thattommyhall.sslify.me')
+    assert_equal '1.2.3.4', get_A('1-2-3-4.thattommyhall.sslocal.io')
+    assert_nil get_A('1-2-3-4.thattommyhall.rando.domain')
+  end
+
+  def test_challenge
+    assert_equal 'SOME-STRING', get_TXT('_acme-challenge.testuser.sslify.me')
   end
 end
